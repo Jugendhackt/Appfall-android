f322506f2eacf2ad4c79896c9e862b3307e87a7c
meetu2603 <44922554+meetu2603@users.noreply.github.com>
2018-11-10 18:18:54 +0100

    esfuktioniert

--- app/src/main/java/com/thinkpaduser/appfall/Einkaufswagen.java
+++ app/src/main/java/com/thinkpaduser/appfall/Einkaufswagen.java
@@ -1,25 +1,61 @@
 package com.thinkpaduser.appfall;
 
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.OnLifecycleEvent;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.firebase.ui.common.ChangeEventType;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.firebase.ui.database.ObservableSnapshotArray;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 public class Einkaufswagen extends AppCompatActivity {
 private FloatingActionButton addItem;
+private ListView einkauf;
+private ListAdapter mAdapter;
+
+
+    Query ref = FirebaseDatabase.getInstance().getReference().child("list");
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_einkaufswagen);
 
         addItem = (FloatingActionButton) findViewById(R.id.btnadditem);
+        einkauf = (ListView) findViewById(R.id.lvEinkauf);
         addItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,14 +63,18 @@ private FloatingActionButton addItem;
                 LayoutInflater inflater = getLayoutInflater();
 View view = inflater.inflate(R.layout.dialog_view_add_item, null, false);
                 final EditText etname = (EditText) view.findViewById(R.id.etproduktname);
-                EditText etmenge = (EditText) view.findViewById(R.id.etmenge);
+                final EditText etmenge = (EditText) view.findViewById(R.id.etmenge);
 
                 builder.setTitle("Hinzufügen")
                         .setView(view)
                         .setPositiveButton("Fertig", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("list");
+
+
+
+
+
 
 
 
@@ -46,6 +86,93 @@ DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("lis
 
 
 
+
         }
     });
-}}
+        listview();
+}
+private void listview(){
+
+    Log.d("list", "hallo");
+
+mAdapter = new ListAdapter();
+    einkauf.setAdapter(mAdapter);
+
+
+}
+
+    public class ListAdapter extends BaseAdapter {
+        private static final String TAG = "FirebaseListAdapter";
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("list");
+
+
+        List<String> namen = Collections.EMPTY_LIST;
+        List<String> mengen = Collections.EMPTY_LIST;
+        public ListAdapter() {
+            ref.addValueEventListener(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                     namen = new ArrayList<>();
+                     mengen = new ArrayList<>();
+                   for (DataSnapshot child : dataSnapshot.getChildren()) {
+
+                       namen.add(child.getKey());
+
+                      mengen.add(
+                              String.valueOf(((HashMap<String,Long>)child.getValue()).get("Menge"))
+                      );
+                       Log.d("line", child.getKey());
+                   }
+                   notifyDataSetChanged();
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+
+        }
+
+
+        @NonNull
+        @Override
+        public String getItem(int position) {
+            return namen.get(position);
+        }
+
+
+        @Override
+        public int getCount() {
+            return namen.size();
+        }
+
+        @Override
+        public long getItemId(int i) {
+            // http://stackoverflow.com/questions/5100071/whats-the-purpose-of-item-ids-in-android-listview-adapter
+            return namen.get(i).hashCode();
+        }
+
+
+        @Override
+        @NonNull
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.listviewitems, parent, false);
+            }
+
+            String model = getItem(position);
+            ((TextView)convertView.findViewById(R.id.tvname)).setText(namen.get(position));
+           ((TextView)convertView.findViewById(R.id.tvmenge)).setText(mengen.get(position));
+
+
+
+            return convertView;
+        }
+    }
+
+}
+
